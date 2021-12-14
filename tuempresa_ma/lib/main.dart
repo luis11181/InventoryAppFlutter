4661e9b9b55812bc8e33774e3769dbeea32a15da
luis11181 <luis11181@hotmail.com>
2021-12-14 07:43:27 -0500

    firebase mapping config

--- tuempresa_ma/lib/main.dart
+++ tuempresa_ma/lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:tuempresa_ma/src/presentation/app.dart';
 import 'package:tuempresa_ma/src/presentation/app_bloc_observer.dart';
 
-void main() {
+Future<void> main() async {
+  //WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   BlocOverrides.runZoned(
     () => runApp(const App()),
     blocObserver: AppBlocObserver(),
   );
 }
+
