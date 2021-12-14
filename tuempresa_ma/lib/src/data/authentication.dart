56e91e19291f4ecc917ef3987676a33a9899faad
luis11181 <luis11181@hotmail.com>
2021-12-14 15:35:35 -0500

    bug min sdk 21

--- tuempresa_ma/lib/src/data/authentication.dart
+++ tuempresa_ma/lib/src/data/authentication.dart
@@ -1,2 +1,20 @@
-
 import 'package:firebase_auth/firebase_auth.dart';
+
+FirebaseAuth auth = FirebaseAuth.instance;
+
+registerUser (number) async { 
+    try {
+    UserCredential userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+      email: "barry.allen@example.com",
+      password: "SuperSecretPassword!"
+    );
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      print('The password provided is too weak.');
+    } else if (e.code == 'email-already-in-use') {
+      print('The account already exists for that email.');
+    }
+  } catch (e) {
+    print(e);
+  }
+}   
