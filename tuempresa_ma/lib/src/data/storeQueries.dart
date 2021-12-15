fe8a36622892fa5b4730a3b262ce14d8d3788191
luis11181 <luis11181@hotmail.com>
2021-12-15 17:26:21 -0500

    funciona nombre empresa desde signin

--- tuempresa_ma/lib/src/data/storeQueries.dart
+++ tuempresa_ma/lib/src/data/storeQueries.dart
@@ -1,23 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 //* query to get the company name from the email of a specific user
-Future<String> getCompanyName(String email) async {
+Future<dynamic> getCompanyName(String email) async {
   // Call the user's CollectionReference to add a new user
-  String companyName;
+  var companyName;
 
   companyName = await FirebaseFirestore.instance
       .collection('empleados')
       .where('email', isEqualTo: email)
       .get()
-      .then((snapshot) {
-    print(snapshot);
-    snapshot.docs.forEach((doc) {
-      print(doc.data());
-    });
-    return snapshot.docs[0].data()['empresa'];
+      .then((snapshot) async {
+        if (snapshot.docs[0].data()["company"] != null) {
+           return await snapshot.docs[0].data()["company"];
+        } else {
+          return null;
+        }  
+
   }).catchError((e) {
     print(e);
-    return 'error';
+    return null;
   });
 
   return companyName;
--- tuempresa_ma/lib/src/presentation/bloc/login_page_bloc/login_page_cubit.dart
+++ tuempresa_ma/lib/src/presentation/bloc/login_page_bloc/login_page_cubit.dart
@@ -21,14 +21,11 @@ class LoginPageCubit extends Cubit<LoginPageState> {
   Future<void> login(BuildContext context) async {
     //* dice que recibe el username, pero se requiere el password por eso se cambio el texto que ve el ususario, uy realmente recibe es el correo, por lo tanto la funcion sign in funciona asi
     bool shouldNavigate = await signIn(state.username, state.password);
-
-    if (shouldNavigate) {
-      //TODO SAVE THE COMPANY NAME AS GLOBAL STATE, SO IT CAN BE USED IN ALL QUERIES, SE CREO LA FUNCION , PERO ESTA NO SIRVE PQ RECIBE UN FUTURO  Y NO UN STRING
-
-      //String companyName = 'ddd'; //await  getCompanyName(state.username);//username es el correo en este caso
-      //getCompanyName(state.username);
+    var companyName = await getCompanyName(state.username);
+    if (shouldNavigate == true && companyName != null) {
+     
       Navigator.pushNamed(context, 'scanpage',
-          arguments: state.username + ' ' + state.password);
+          arguments: state.username + ' ' + state.password + ' ' + companyName);
     } else {
       //TODO show error IN SCREEN
     }
