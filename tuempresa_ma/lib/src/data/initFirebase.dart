bc9fa4d93c25326ebd3878af5ffc96ae09268481
Sergio Sarmiento <sisarmientob@unal.edu.co>
2022-02-06 17:05:08 -0500

    Se arregla funcion de iniciarProductos

--- tuempresa_ma/lib/src/data/initFirebase.dart
+++ tuempresa_ma/lib/src/data/initFirebase.dart
@@ -3,20 +3,23 @@ import 'package:tuempresa_ma/src/data/initFirebase.dart';
 import 'package:tuempresa_ma/src/data/queriesFirestore.dart';
 
 //dbRef.collection("Table").where("exObject.dataToQuery", "==", "value")
-CollectionReference empresas = FirebaseFirestore.instance.collection('empresasf');
+CollectionReference empresas =
+    FirebaseFirestore.instance.collection('empresasf');
 
 Future<void> addUser(String name, String lastName, String company, String email,
     String user) async {
   // Call the user's CollectionReference to add a new user
-  return empresas.doc(company)
-  .collection("empleados").doc(email)
+  return empresas
+      .doc(company)
+      .collection("empleados")
+      .doc(email)
       .set(
         {
-        'name': name, //
-        'lastName': lastName, // John
-        'email': email, //
-        'user': user, //
-      },
+          'name': name, //
+          'lastName': lastName, // John
+          'email': email, //
+          'user': user, //
+        },
         SetOptions(merge: true), // previous data is not deleted
       )
       .then((value) => print("User Added"))
@@ -26,8 +29,8 @@ Future<void> addUser(String name, String lastName, String company, String email,
 //dbRef.collection("Table").where("exObject.dataToQuery", "==", "value")
 CollectionReference users = FirebaseFirestore.instance.collection('empleados');
 
-Future<void> addUser2(String name, String lastName, String company, String email,
-    String user) async {
+Future<void> addUser2(String name, String lastName, String company,
+    String email, String user) async {
   // Call the user's CollectionReference to add a new user
   return users
       .add({
@@ -46,18 +49,19 @@ Future<void> iniciarProductos(
   return empresas
       // existing document in 'users' collection: "ABC123"
       .doc(company)
-      .collection("productos").doc("12SDF")
+      .collection("productos")
+      .doc("12SDF")
       .set(
         {
-        'cantidad': 0, //
-        'desccripcion': "no hay", // John
-        'nombre': 'producto-prueba', // //
-        'productBarcode': "12SDF",
-      },
+          'cantidad': 0, //
+          'descripcion': "no hay", // John
+          'nombre': 'producto-prueba', // //
+          'productBarcode': "12SDF",
+          'unidad': "cajas",
+          'precio': 4000,
+        },
         SetOptions(merge: true), // previous data is not deleted
       )
       .then((value) => print("'full_name' & 'age' merged with existing data!"))
       .catchError((error) => print("Failed to merge data: $error"));
 }
-
-
