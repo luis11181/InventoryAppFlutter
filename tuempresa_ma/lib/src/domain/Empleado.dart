d0f6090da84384ab2ab59b5bf4470439e84dcd51
Gabriel Acevedo <gaacevedof@unal.edu.co>
2022-02-05 23:02:17 -0500

    Creación carpeta domain

---
+++ tuempresa_ma/lib/src/domain/Empleado.dart
@@ -0,0 +1,15 @@
+
+class Empleado {
+
+  Empleado({
+  this.nombre = '',
+  this.apellido = '',
+  this.usuario = '',
+  this.correo = '',
+  });
+
+  String nombre;
+  String apellido;
+  String usuario;
+  String correo;
+}
